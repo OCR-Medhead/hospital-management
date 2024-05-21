2a07707479ffa2d12a45bcde2edd03b905d3eb20
heyveryouaire <mathieu.pavy.29@gmail.com>
2024-05-21 10:57:44 +0200

    update testing

--- front/src/app/service/auth.service.spec.ts
+++ front/src/app/service/auth.service.spec.ts
@@ -1,10 +1,11 @@
-import { TestBed, fakeAsync, flush, tick } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AuthService } from './auth.service';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('AuthService', () => {
-    let service: AuthService;
+    let httpClientSpy: jasmine.SpyObj<HttpClient>;
+    let authService: AuthService;
 
     beforeEach(() => {
         jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
@@ -12,29 +13,33 @@ describe('AuthService', () => {
             imports: [HttpClientModule],
             providers: [HttpClientModule],
         })
-        service = TestBed.inject(AuthService);
+
+        httpClientSpy = jasmine.createSpyObj('HttpClient', ['post', 'get']);
+        authService = new AuthService(httpClientSpy);
+
     });
 
     it('should be created', () => {
-        expect(service).toBeTruthy();
+        expect(authService).toBeTruthy();
     });
 
-    it("Login should failed", () => {
-        console.log("test")
+    it("Login should failed", async () => {
+        httpClientSpy.post.and.returnValue(of({status:403}))      
 
-        console.debug("debug test")
-        let reponse = new Promise<boolean>(async (resolve, reject) => {
-            resolve(await service.login("zdazd", "azd"))
+        let reponse = new Promise<boolean>(async (resolve, _) => {
+            resolve(await authService.login("zdazd", "azd"))
         })
         expect(reponse).not.toBeTrue()
     })
 
     it("Login should success", (done) => {
-        service.login("dev@dev.fr", "123").then((value) => {
+        httpClientSpy.post.and.returnValue(of("eyhbzfzebnfi"))
+
+        authService.login("dev@dev.fr", "123").then((value) => {
             expect(value).toBeTrue()
 
-            let token = service.loadTokenInStorage()
-            expect(token).not.toEqual("")
+            let token = authService.loadTokenInStorage()
+            expect(token).toEqual("eyhbzfzebnfi")
             done()
         })
 
