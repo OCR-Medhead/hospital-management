871d0f37899bb15adb9cf0129a67b025f6155d62
heyveryouaire <mathieu.pavy.29@gmail.com>
2024-06-04 17:08:25 +0200

    update port

--- front/src/app/service/auth.service.ts
+++ front/src/app/service/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, firstValueFrom } from 'rxjs';
 
-const  API_HOST_URI = "http://localhost:8888/"
+const  API_HOST_URI = "http://localhost:8080/"
 
 
 @Injectable({
